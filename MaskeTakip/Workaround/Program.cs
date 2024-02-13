1d5601439281d3696582d514b150e291c1d2102b
Mehmet Aydın <54308525+kocaeliproje@users.noreply.github.com>
2024-02-13 18:47:30 +0300

    Maske Takip
    Kodlama.io C# ders 1 ödev 2

---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Ali";
+            person1.LastName = "Soy";
+            person1.DateOfBirthday = 1980;
+            person1.NationalIdentity = 1234567;
+
+            Console.ReadLine();
+        
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+
+        Console.ReadLine();
+        }
+    }
+}
